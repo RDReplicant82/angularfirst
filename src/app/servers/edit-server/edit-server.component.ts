a7a0b440612f6d0ff6d22c2e22fbe86df17781ee
David Arochena <david.ar1982@gmail.com>
2018-05-14 17:17:47 +0200

    new funcionality added

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,27 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription, Observable } from 'rxjs';
+import { CanComponentDeactivate } from './can-deactivated-guard.service';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, OnDestroy, CanComponentDeactivate {
+  onServerChanged: Subscription;
+  onQueryParamsChanged: Subscription;
   server: {id: number, name: string, status: string};
+  allowEdit = false;
+  changesSaved = false;
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, 
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     this.server = this.serversService.getServer(1);
+
+    this.onServerChanged = this.route.params.subscribe(
+      (params: Params) => {
+        this.server = (params.id) ? this.serversService.getServer(parseInt(params.id)) : this.serversService.getServer(1);
+      }
+    );
+
+    this.onQueryParamsChanged = this.route.queryParams.subscribe(
+      (queryParams: Params) => {
+        this.allowEdit = queryParams['allowEdit'] === '1';
+      }
+    );
+
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
 
+  ngOnDestroy() {
+    this.onServerChanged.unsubscribe();
+    this.onQueryParamsChanged.unsubscribe();
+  }
+
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
+    this.router.navigate(['../'], { relativeTo : this.route })
+  }
+
+  canDeactivate() : Observable<boolean> | Promise<boolean> | boolean {
+    if(!this.allowEdit) {
+      return true;
+    }
+    if((this.serverName !== this.server.name || this.serverStatus !== this.server.status) &&
+        !this.changesSaved) {
+          return confirm('Do you want to discard changes?');
+        } else {
+          return true;
+        }
   }
 
 }
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -9,11 +11,33 @@ import { ServersService } from '../servers.service';
 })
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
+  serverId: Subscription;
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService, 
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    this.route.data
+      .subscribe(
+        (data: Data) => {
+          this.server = data['server'];
+        }
+      );
+/*     this.server = this.serversService.getServer(1);
+
+    this.serverId = this.route.params.subscribe(
+      (params: Params) => {
+        if(params.id) {
+          this.server = this.serversService.getServer(parseInt(params.id));
+        }
+      }
+    ) */
+  }
+
+  onEdit(serverIdToLoad: number) {
+    this.router.navigate(['edit'], { relativeTo : this.route, queryParamsHandling: 'preserve'});
   }
 
 }
